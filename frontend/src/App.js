fee5f917f3d94b4e9abe4bca9fef464ff4bb04a8
Digvijaysingh0707 <digvijaysinghonly1@gmail.com>
2024-07-22 09:56:36 +0530

    context api

---
+++ frontend/src/App.js
@@ -0,0 +1,18 @@
+import React, { createContext, useState } from 'react'
+import TaskRoutes from './TaskRoutes'
+import { ToastContainer } from 'react-toastify'
+
+export const TaskContext = createContext()
+
+const App = () => {
+  const [userDetails, setUserDetails] = useState()
+
+  return (
+    <TaskContext.Provider value={{ userDetails, setUserDetails }}>
+      <TaskRoutes />
+      <ToastContainer />
+    </TaskContext.Provider>
+  )
+}
+
+export default App
