6def0fc15d8cadbf9b625fdb10c0bb6467cf5d5e
Digvijaysingh0707 <digvijaysinghonly1@gmail.com>
2024-07-22 12:21:12 +0530

    add login as default url

--- frontend/src/TaskRoutes.js
+++ frontend/src/TaskRoutes.js
@@ -1,10 +1,9 @@
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
-import React, { createContext, useState } from "react";
+import React from "react";
 import TaskManagement from "./components/TaskManagement";
 import Signup from "./components/Signup";
 import LoginPage from "./components/LoginPage";
 import Appbar from "./components/Appbar";
-
 
 const TaskRoutes = () => {
   const userToken = localStorage.getItem("token");
@@ -19,6 +18,7 @@ const TaskRoutes = () => {
         />
         <Route path="/signup" element={<Signup />} />
         <Route path="/login" element={<LoginPage />} />
+        <Route path="*" element={<Navigate to="/login" />} />
       </Routes>
     </BrowserRouter>
   );
